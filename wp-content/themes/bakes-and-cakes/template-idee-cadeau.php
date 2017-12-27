63dbaf09c23bcc6c4c1ddfefaeab9c7240e42949
Karim Morel <karim-morel@outlook.fr>
2017-12-27 23:36:40 +0100

    Page idée cadeau

---
+++ wp-content/themes/bakes-and-cakes/template-idee-cadeau.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Template Name: Idée cadeaux page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Bakes And Cakes
+ */
+
+get_header();
+
+?>
+<section class="featured" id="product">
+                
+<div class="container">
+
+<header class="header"><h1 class="main-title">Besoin d'une idée cadeau ?</h1><p>Retrouvez des apéritifs et sirops artisanaux en ligne.</p>
+</header>			<ul class="featured-slider">
+							<li>
+					<div class="img-holder">
+						<a href="http://jardindesmalices.com/commande/">
+						     <img width="235" height="235" src="<?php echo get_template_directory_uri() ?>/images/idee-cadeau/sirop.jpg" class="attachment-bakes-and-cakes-product-thumb size-bakes-and-cakes-product-thumb wp-post-image" sizes="(max-width: 235px) 100vw, 235px" />						</a>
+					 </div>
+					<div class="text-holder">
+						<a href="http://jardindesmalices.com/commande/"><strong class="name">Sirop de fleur de sureau 20cl</strong></a>
+						<span class="price">
+						    <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol"></span>5.50 &#8364;</span></span>
+						
+					</div>
+				</li>
+							<li>
+					<div class="img-holder">
+						<a href="http://jardindesmalices.com/commande/">
+						     <img width="235" height="235" src="<?php echo get_template_directory_uri() ?>/images/idee-cadeau/sureau.jpg" class="attachment-bakes-and-cakes-product-thumb size-bakes-and-cakes-product-thumb wp-post-image" sizes="(max-width: 235px) 100vw, 235px" />							</a>
+					 </div>
+					<div class="text-holder">
+						<a href="http://jardindesmalices.com/commande/"><strong class="name">Apéritif à la fleur de sureau 20cl</strong></a>
+						<span class="price">
+						    <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol"></span>7.50 &#8364;</span>						</span>
+						
+					</div>
+				</li>
+							<li>
+					<div class="img-holder">
+						<a href="http://jardindesmalices.com/commande/">
+						     <img width="235" height="235" src="<?php echo get_template_directory_uri() ?>/images/idee-cadeau/prunellier.jpg" class="attachment-bakes-and-cakes-product-thumb size-bakes-and-cakes-product-thumb wp-post-image" sizes="(max-width: 235px) 100vw, 235px" />						</a>
+					 </div>
+					<div class="text-holder">
+						<a href="http://jardindesmalices.com/commande/"><strong class="name">Apéritif de prunellier sauvage 20cl</strong></a>
+						<span class="price">
+						    <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol"></span>7.50 &#8364;</span>						</span>
+						
+					</div>
+				</li>
+							<li>
+					<div class="img-holder">
+						<a href="http://jardindesmalices.com/commande/">
+						     <img width="235" height="235" src="<?php echo get_template_directory_uri() ?>/images/idee-cadeau/noisette.jpg" class="attachment-bakes-and-cakes-product-thumb size-bakes-and-cakes-product-thumb wp-post-image" sizes="(max-width: 235px) 100vw, 235px" />						</a>
+					 </div>
+					<div class="text-holder">
+						<a href="http://jardindesmalices.com/commande/"><strong class="name">Apéritif à la noisette 20cl</strong></a>
+						<span class="price">
+						    <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol"></span>7.50 &#8364;</span>						</span>
+						
+					</div>
+				</li>
+							<li>
+					<div class="img-holder">
+						<a href="http://jardindesmalices.com/commande/">
+						     <img width="235" height="235" src="<?php echo get_template_directory_uri() ?>/images/idee-cadeau/prunellier.jpg" class="attachment-bakes-and-cakes-product-thumb size-bakes-and-cakes-product-thumb wp-post-image" sizes="(max-width: 235px) 100vw, 235px" />						</a>
+					 </div>
+					<div class="text-holder">
+						<a href="http://jardindesmalices.com/commande/"><strong class="name">Apéritif de prunellier sauvage 70cl</strong></a>
+						<span class="price">
+						    <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol"></span>17.00 &#8364;</span>						</span>
+						
+					</div>
+				</li>
+						</ul>
+	</div>
+            </section>
+
+
+
+            <?php 
+
+            get_footer();
+
+            ?>
